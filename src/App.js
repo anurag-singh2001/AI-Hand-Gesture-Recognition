625c21b84ee5b88acc102a6dd45fa7cc5269383e
anurag-singh2001 <anuragrazarwal@gmail.com>
2022-07-03 20:15:05 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,169 @@
+// 0. Install fingerpose npm install fingerpose
+// 1. Add Use State
+// 2. Import emojis and finger pose import * as fp from "fingerpose";
+// 3. Setup hook and emoji object
+// 4. Update detect function for gesture handling
+// 5. Add emoji display to the screen
+
+///////// NEW STUFF ADDED USE STATE
+import React, { useRef, useState, useEffect } from "react";
+///////// NEW STUFF ADDED USE STATE
+
+// import logo from './logo.svg';
+import * as tf from "@tensorflow/tfjs";
+import * as handpose from "@tensorflow-models/handpose";
+import Webcam from "react-webcam";
+import "./App.css";
+import { drawHand } from "./utilities";
+
+///////// NEW STUFF IMPORTS
+import * as fp from "fingerpose";
+import * as fpg from "fingerpose-gestures";
+import victory from "./victory.png";
+import thumbs_up from "./thumbs_up.png";
+import raised_hand from "./raised_hand.jpg";
+import thumbs_down from "./thumbs_down.png";
+import ok from "./ok.jpg";
+import fist from "./fist.jpg";
+///////// NEW STUFF IMPORTS
+
+function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  ///////// NEW STUFF ADDED STATE HOOK
+  const [emoji, setEmoji] = useState(null);
+  const images = { thumbs_up: thumbs_up, victory: victory, raised_hand: raised_hand,thumbs_down: thumbs_down, ok: ok, fist: fist };
+  ///////// NEW STUFF ADDED STATE HOOK
+
+  const runHandpose = async () => {
+    const net = await handpose.load();
+    console.log("Handpose model loaded.");
+    //  Loop and detect hands
+    setInterval(() => {
+      detect(net);
+    }, 10);
+  };
+
+  const detect = async (net) => {
+    // Check data is available
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Set canvas height and width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      // Make Detections
+      const hand = await net.estimateHands(video);
+      // console.log(hand);
+
+      ///////// NEW STUFF ADDED GESTURE HANDLING
+
+      if (hand.length > 0) {
+        const GE = new fp.GestureEstimator([
+          fp.Gestures.VictoryGesture,
+          fp.Gestures.ThumbsUpGesture,
+          fpg.Gestures.raisedHandGesture,
+          fpg.Gestures.thumbsDownGesture,
+          fpg.Gestures.okGesture,
+          fpg.Gestures.fistGesture
+        ]);
+        const gesture = await GE.estimate(hand[0].landmarks, 4);
+        if (gesture.gestures !== undefined && gesture.gestures.length > 0) {
+          console.log(gesture);
+
+          const score = gesture.gestures.map(
+            (prediction) => prediction.score
+          );
+          //console.log(confidence);
+          const maxScore = score.indexOf(
+            Math.max.apply(null, score)
+          );
+          //console.log(maxConfidence);
+          console.log(gesture.gestures[maxScore].name);
+          //console.log(gesture);
+          setEmoji(gesture.gestures[maxScore].name);
+          //console.log(emoji);
+        }
+      }
+
+      ///////// NEW STUFF ADDED GESTURE HANDLING
+
+      // Draw mesh
+      const ctx = canvasRef.current.getContext("2d");
+      drawHand(hand, ctx);
+    }
+  };
+
+  useEffect(()=>{runHandpose()},[]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+        {/* NEW STUFF */}
+        {emoji !== null ? (
+          <img
+            src={images[emoji]}
+            style={{
+              position: "absolute",
+              marginLeft: "auto",
+              marginRight: "auto",
+              left: 400,
+              bottom: 500,
+              right: 0,
+              textAlign: "center",
+              height: 100,
+            }}
+          />
+        ) : (
+          ""
+        )}
+
+        {/* NEW STUFF */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
